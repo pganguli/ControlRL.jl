bd3eb6c7a6df590c0f6c7caf21d5bcccdbd46c3f
Prateek Ganguli <prateek.ganguli@gmail.com>
2025-06-30 22:11:46 -0400

    Add RL paper notebooks

---
+++ examples/rl_DC.jl
@@ -0,0 +1,149 @@
+### A Pluto.jl notebook ###
+# v0.20.10
+
+using Markdown
+using InteractiveUtils
+
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    #! format: off
+    return quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
+        el
+    end
+    #! format: on
+end
+
+# ╔═╡ 04f3e318-6489-11ef-1ccc-89d0ed2d1029
+begin
+  using Pkg
+  Pkg.activate(".")
+  Pkg.instantiate()
+
+  using Revise
+  using LinearAlgebra
+  using PlutoUI
+  using Plots
+  #plotlyjs()
+
+  push!(LOAD_PATH, "$(@__DIR__)/../src")
+  using ControlRL
+
+  TableOfContents()
+end
+
+# ╔═╡ abb82687-12f0-4754-b3dd-49c03f3c9ad1
+md"""
+## Import Packages
+"""
+
+# ╔═╡ 5ff9a070-12a3-45fd-a1b4-b619998e468a
+md"""
+## Define policy π
+"""
+
+# ╔═╡ e8996f64-b030-4624-9e34-d452e5d2d5db
+begin
+  const safety_margin = 1.5
+  const H = 800
+  const Tₛ = 0.02
+  const γ = 0.99
+  const α = 0.35
+  @bind dₜ_optimal Slider(0:0.001:1, show_value=true, default=0.052)
+end
+
+# ╔═╡ fac6522b-c189-4b7c-be35-11d038a854bc
+π(actions::Vector{Bool}, states::Vector{Vector{Float64}}, distances::Vector{Float64}, dₜ::Float64) = (length(actions) == 0) || (distances[end] > dₜ * safety_margin)
+
+# ╔═╡ 4798f6c2-ce68-4818-8a84-75323bdd8306
+dₜ_optimal
+
+# ╔═╡ e705f0df-bdcc-4673-976f-5d096c6a2abc
+md"""
+## Helper Functions
+"""
+
+# ╔═╡ 5402045e-83ef-4b90-9f14-1796fdadacc0
+function plotH(states::Matrix{Float64})
+  # Transposing `states` because plot uses the first dimension as indices.
+  #plot(0:size(states, 2)-1, states')
+  plot(0:size(states, 2)-1, states', xlabel="Iteration", ylabel="System Dynamics", xguidefontsize=22, yguidefontsize=22, legendfontsize=12, legend=:bottomright, xtickfontsize=14, ytickfontsize=14)
+  #savefig("figure.svg")
+end
+
+# ╔═╡ c34375b4-70ab-4468-9186-c43948f22a7a
+plotH(v::Vector{Float64}) = plotH(reshape(v, 1, :))
+
+# ╔═╡ 843ed7d1-692c-4579-911d-32534a167280
+function reward(distances::Vector{Float64}, utilizations::Vector{Float64}, α::Float64, γ::Float64, H::Int64)
+	discounted_rewards = [-1 * (γ^i) * (α * distances[i] + (1 - α) * utilizations[i]) for i = 1:H]
+    return discounted_rewards, sum(discounted_rewards)
+end
+
+# ╔═╡ 2d1d69f5-aed9-4f99-a63a-2f94347c7e15
+begin
+  sys_discrete = c2d(benchmarks[:DC], Tₛ)
+  env = Environment(sys_discrete)
+
+ #  dₜ_optimal = 0
+ #  max_total_reward = -999
+ #  for dₜ = 0:0.001:1
+ #    actions, states, distances, utilizations, ideal_states = sim!(env, π, dₜ, H)
+	# discounted_rewards, total_reward = reward(distances, utilizations, α, γ, H)
+
+	# if max_total_reward < total_reward
+	#   max_total_reward = total_reward
+	#   dₜ_optimal = dₜ
+	# end
+ #  end
+
+  actions, states, distances, utilizations, ideal_states = sim!(env, π, dₜ_optimal, H)
+  discounted_rewards, total_reward = reward(distances, utilizations, α, γ, H)
+end
+
+# ╔═╡ d58e85a8-513f-45e2-906c-41d3bdea7bb5
+sum(actions)
+
+# ╔═╡ 036b19be-0900-43cf-b8b9-e746ed1a8c0d
+actions
+
+# ╔═╡ 19634c38-ec2c-45a0-862a-3f92beecb5a1
+plotH(ideal_states)
+
+# ╔═╡ 66fbe369-33e7-409f-ae66-29c1984518b3
+plotH(states)
+
+# ╔═╡ 7c54f79a-6bb4-4190-ab13-bca71bb868a8
+plotH(distances)
+
+# ╔═╡ 022d0123-0051-4815-8dda-0611edfceba6
+plotH(utilizations)
+
+# ╔═╡ 2d1ae051-9ea6-4525-9a53-769c9849915f
+plotH(discounted_rewards)
+
+# ╔═╡ 10033ad2-b054-4444-bf3e-25d0ac0810ec
+maximum(distances[1:end])
+
+# ╔═╡ Cell order:
+# ╟─abb82687-12f0-4754-b3dd-49c03f3c9ad1
+# ╠═04f3e318-6489-11ef-1ccc-89d0ed2d1029
+# ╟─5ff9a070-12a3-45fd-a1b4-b619998e468a
+# ╠═fac6522b-c189-4b7c-be35-11d038a854bc
+# ╠═e8996f64-b030-4624-9e34-d452e5d2d5db
+# ╠═2d1d69f5-aed9-4f99-a63a-2f94347c7e15
+# ╠═4798f6c2-ce68-4818-8a84-75323bdd8306
+# ╠═d58e85a8-513f-45e2-906c-41d3bdea7bb5
+# ╠═036b19be-0900-43cf-b8b9-e746ed1a8c0d
+# ╠═19634c38-ec2c-45a0-862a-3f92beecb5a1
+# ╠═66fbe369-33e7-409f-ae66-29c1984518b3
+# ╠═7c54f79a-6bb4-4190-ab13-bca71bb868a8
+# ╠═022d0123-0051-4815-8dda-0611edfceba6
+# ╠═2d1ae051-9ea6-4525-9a53-769c9849915f
+# ╠═10033ad2-b054-4444-bf3e-25d0ac0810ec
+# ╟─e705f0df-bdcc-4673-976f-5d096c6a2abc
+# ╠═5402045e-83ef-4b90-9f14-1796fdadacc0
+# ╠═c34375b4-70ab-4468-9186-c43948f22a7a
+# ╠═843ed7d1-692c-4579-911d-32534a167280
